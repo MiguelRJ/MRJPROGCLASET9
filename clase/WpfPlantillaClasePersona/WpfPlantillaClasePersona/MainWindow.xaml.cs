2cf24b0b6233caa76bd62a4d8087ce36b175dfe7
MiguelRJ <miguel.rj96@gmail.com>
2017-05-17 16:02:59 +0200

    05-17 Ultimos ejercicios por hacer

---
+++ clase/WpfPlantillaClasePersona/WpfPlantillaClasePersona/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//-----
+using Migue.Wpf_EnlacesDataContextAClases;
+
+namespace WpfPlantillaClasePersona
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ListaPersonas _listaPersonas = null;
+        int pos = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _listaPersonas = new ListaPersonas();
+            grdDatos.DataContext = _listaPersonas;
+        }
+
+        private void btnSiguiente_Click(object sender, RoutedEventArgs e)
+        {
+            Persona tmpPersona = _listaPersonas[pos++%_listaPersonas.Count];
+            grdDatos.DataContext = tmpPersona;
+        }
+    }
+}
